84aea3780a77eccda786ed8c12ef9f0281e4caf5
Luca <lucadeste21@gmail.com>
2022-06-17 00:43:30 +0200

    added possibility to change color + style

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const container = document.getElementById('container');
 const pixelLabel = document.getElementById('pixelLabel');
 const pixelInput = document.getElementById('pixelSize');
+const ColorInput = document.getElementById('color');
+let color = ColorInput.value;
 let pixelSize = 16;
-let mouseDown = 0;
 pixelLabel.innerText =`Pixel Size : ${pixelSize}`
 pixelInput.value = pixelSize;
 pixelInput.onchange = function(){
     console.log(this.value)
     pixelSize = this.value;
+    pixelLabel.innerText =`Pixel Size : ${pixelSize}`
     createGrid(pixelSize);
+}
+ColorInput.onchange = function(){
+    color = this.value;
 }
 
 function vw(v) {
@@ -16,11 +21,8 @@ function vw(v) {
     return (v * w) / 100;
   }
 
-window.addEventListener('mousedown',e =>{
-    mouseDown=1;
-})
-window.addEventListener('mouseup',e=>{
-    mouseDown=0;
+window.addEventListener('resize',e=>{
+    createGrid(pixelSize);
 })
 
 function createGrid(pxl){
@@ -34,10 +36,14 @@ function createGrid(pxl){
         drawable.style.height=`${square}px`;
         drawable.classList.add('hover');
         drawable.addEventListener('mouseover',e => {
-            if(mouseDown>0){
-                drawable.style.backgroundColor='black';
+            if(e.buttons == 1 || e.buttons == 3){
+                drawable.style.backgroundColor=color;
             }
         })
+        drawable.addEventListener('mousedown',e => {
+                drawable.style.backgroundColor=color;
+        })
+
         container.appendChild(drawable);
     }
 }
